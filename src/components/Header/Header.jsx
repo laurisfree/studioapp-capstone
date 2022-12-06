1935b39e02758e98dd0c210c5a6a27953a181d5d
laurisfree <freelau00@gmail.com>
2022-12-05 17:22:15 -0800

    header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,27 +6,27 @@ export default function Header() {
 	return (
 		<>
             <div className="header-main">
-                    <div className="header-main__img-wrp">
-                        <img src={logo} alt="" className='header-main__img'/>
+                <div className="header-main__img-wrp">
+                    <img src={logo} alt="" className='header-main__img'/>
+                </div>
+                <div className='header-main__wrp-nav'>
+                    <div className="header-main__nav">
+                        <img src="" alt="" />
+                        <p>Home</p>
                     </div>
-                    <div className='header-main__wrp-nav'>
-                        <div className="header-main__nav">
-                            <img src="" alt="" />
-                            <p>Home</p>
-                        </div>
-                        <div className="header-main__nav">
-                            <img src="" alt="" />
-                            <p>Book</p>
-                        </div>
-                        <div className="header-main__nav">
-                            <img src="" alt="" />
-                            <p>Buy</p>
-                        </div>
-                        <div className="header-main__nav">
-                            <img src="" alt="" />
-                            <p>Profile</p>
-                        </div>
+                    <div className="header-main__nav">
+                        <img src="" alt="" />
+                        <p>Book</p>
                     </div>
+                    <div className="header-main__nav">
+                        <img src="" alt="" />
+                        <p>Buy</p>
+                    </div>
+                    <div className="header-main__nav">
+                        <img src="" alt="" />
+                        <p>Profile</p>
+                    </div>
+                </div>
             </div>
 		</>
 	);
