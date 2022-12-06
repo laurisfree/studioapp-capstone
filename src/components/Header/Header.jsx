1c457a3978a0637b95006663f5c95f09830f4576
laurisfree <freelau00@gmail.com>
2022-12-05 16:49:12 -0800

    header syled-mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,31 @@
 import './Header.scss';
-import logo from '../../assets/logo/logo-ds.png'
+import logo from '../../assets/logo/logo-dance-studio.png'
 
 
 export default function Header() {
 	return (
 		<>
-            <div className="header">
-                    <div className="header__img-wrp">
-                        <img src={logo} alt="" />
+            <div className="header-main">
+                    <div className="header-main__img-wrp">
+                        <img src={logo} alt="" className='header-main__img'/>
                     </div>
-                    <div className="header__nav">
-                        <img src="" alt="" />
-                        <p>Home</p>
-                    </div>
-                    <div className="header__nav">
-                        <img src="" alt="" />
-                        <p>Book</p>
-                    </div>
-                    <div className="header__nav">
-                        <img src="" alt="" />
-                        <p>Buy</p>
-                    </div>
-                    <div className="header__nav">
-                        <img src="" alt="" />
-                        <p>Profile</p>
+                    <div className='header-main__wrp-nav'>
+                        <div className="header-main__nav">
+                            <img src="" alt="" />
+                            <p>Home</p>
+                        </div>
+                        <div className="header-main__nav">
+                            <img src="" alt="" />
+                            <p>Book</p>
+                        </div>
+                        <div className="header-main__nav">
+                            <img src="" alt="" />
+                            <p>Buy</p>
+                        </div>
+                        <div className="header-main__nav">
+                            <img src="" alt="" />
+                            <p>Profile</p>
+                        </div>
                     </div>
             </div>
 		</>
